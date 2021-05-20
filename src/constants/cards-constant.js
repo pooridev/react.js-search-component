1999630d2c450d4baafe1f22726f09fbe95c0add
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-05-21 03:12:45 +0430

    Feat: Render subcomponents in App.js

--- src/constants/cards-constant.js
+++ src/constants/cards-constant.js
@@ -1,8 +1,12 @@
 export const CARDS_DETAILTS = [
-  { title: 'Boat', image: 'assets/icons/boat.svg' },
-  { title: 'Cars', image: 'assets/icons/cars.svg' },
-  { title: 'Bike', image: 'assets/icons/bike.svg' },
-  { title: 'Property', image: 'assets/icons/property.svg' },
-  { title: 'Ticket', image: 'assets/icons/ticket.svg' },
-  { title: 'Economy', image: 'assets/icons/economy.svg' }
+  { title: 'Boat', image: 'assets/icons/boat.svg', themeColor: '#ff8faa' },
+  { title: 'Cars', image: 'assets/icons/cars.svg', themeColor: '#8fbcff' },
+  { title: 'Bike', image: 'assets/icons/bike.svg', themeColor: '#a6cd74' },
+  {
+    title: 'Property',
+    image: 'assets/icons/property.svg',
+    themeColor: '#a68fff'
+  },
+  { title: 'Ticket', image: 'assets/icons/ticket.svg', themeColor: '#fcc18a' },
+  { title: 'Economy', image: 'assets/icons/economy.svg', themeColor: '#6edddd' }
 ];
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,14 @@
+import Cards from '../components/Cards';
+import Search from '../components/Search';
+import classes from './App.module.css';
+
 function App() {
-  return <></>;
+  return (
+    <div className={classes.App}>
+      <Search />
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
