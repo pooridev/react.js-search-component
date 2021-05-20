13f421b94b809e044bbabe00a34d369d4583ccee
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-05-21 00:28:05 +0430

    Feat: Add cards constant

---
+++ src/constants/cards-constant.js
@@ -0,0 +1,8 @@
+export const CARDS_DETAILTS = [
+  { title: 'Boat', image: 'assets/icons/boat.svg' },
+  { title: 'Cars', image: 'assets/icons/cars.svg' },
+  { title: 'Bike', image: 'assets/icons/bike.svg' },
+  { title: 'Property', image: 'assets/icons/property.svg' },
+  { title: 'Ticket', image: 'assets/icons/ticket.svg' },
+  { title: 'Economy', image: 'assets/icons/economy.svg' }
+];
